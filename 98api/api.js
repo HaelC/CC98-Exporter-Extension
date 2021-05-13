b7cc766850709dc45efa9d21af2139d1e0057fbb
Hael Chan <f.procumbens@gmail.com>
2021-05-12 21:47:06 -0700

    move post-related functions to api.js, optimize return condition in getAllPosts

--- 98api/api.js
+++ 98api/api.js
@@ -1,5 +1,46 @@
 const BASE_URL = "https://api.cc98.org";
-export const TOPIC_URL = `${BASE_URL}/topic`;
-export const hotPostsURL = (topicId) => `${BASE_URL}/Topic/${topicId}/hot-post`;
-export const allPostsURL = (topicId, start) =>
+const TOPIC_URL = `${BASE_URL}/topic`;
+const hotPostsURL = (topicId) => `${BASE_URL}/Topic/${topicId}/hot-post`;
+const allPostsURL = (topicId, start) =>
   `${BASE_URL}/Topic/${topicId}/post?from=${start}&size=10`;
+
+export async function getTopic(topic) {
+  const topicId = topic.id;
+  const response = await fetch(`${TOPIC_URL}/${topicId}`);
+  if (response.status === 200) {
+    const result = await response.json();
+    const { hitCount, replyCount, title, time } = result;
+    // console.log(hitCount, replyCount, title, time);
+    topic.hitCount = hitCount;
+    topic.replyCount = replyCount;
+    topic.title = title;
+    topic.time = time;
+  }
+}
+
+export async function getHotPosts(topic) {
+  const topicId = topic.id;
+  const response = await fetch(hotPostsURL(topicId));
+  if (response.status === 200) {
+    const result = await response.json();
+    // console.log(result);
+    topic.hotPosts = result;
+  }
+}
+
+export async function getAllPosts(topic) {
+  let start = 0;
+  const topicId = topic.id;
+  while (true) {
+    const response = await fetch(allPostsURL(topicId, start));
+    if (response.status === 200) {
+      const result = await response.json();
+      // console.log(result);
+      topic.posts = topic.posts.concat(result);
+      if (result.length < 10) {
+        return;
+      }
+      start += 10;
+    }
+  }
+}
--- popup/popup.js
+++ popup/popup.js
@@ -1,4 +1,4 @@
-import { TOPIC_URL, hotPostsURL, allPostsURL } from "../98api/api.js";
+import { getTopic, getHotPosts, getAllPosts } from "../98api/api.js";
 
 function constructOptions() {
   chrome.storage.sync.get(["pseudonym", "fileFormat", "range"], (data) => {
@@ -29,41 +29,3 @@ form.addEventListener("submit", async (e) => {
   // Promise.all() to run async functions in parallel.
   console.log(topic);
 });
-
-async function getTopic(topic) {
-  const response = await fetch(`${TOPIC_URL}/${topicId}`);
-  if (response.status === 200) {
-    const result = await response.json();
-    const { hitCount, replyCount, title, time } = result;
-    // console.log(hitCount, replyCount, title, time);
-    topic.hitCount = hitCount;
-    topic.replyCount = replyCount;
-    topic.title = title;
-    topic.time = time;
-  }
-}
-
-async function getHotPosts(topic) {
-  const response = await fetch(hotPostsURL(topicId));
-  if (response.status === 200) {
-    const result = await response.json();
-    // console.log(result);
-    topic.hotPosts = result;
-  }
-}
-
-async function getAllPosts(topic) {
-  let start = 0;
-  while (true) {
-    const response = await fetch(allPostsURL(topicId, start));
-    if (response.status === 200) {
-      const result = await response.json();
-      // console.log(result);
-      if (result.length === 0) {
-        return;
-      }
-      topic.posts = topic.posts.concat(result);
-      start += 10;
-    }
-  }
-}
